334184b40bef476d884318108e7a7ec4c3267027
Dmitry Nikolaev <dnikolaev@fastmail.com>
2020-11-04 23:13:34 +0200

    New binaries and build script.

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #! /bin/sh
-csc -O5 -static src/main.scm -o bin/bib2json
+csc -c -static src/main.scm -o main.o
+csc -c -static src/server.scm -o server.o
+csc -c -static src/libparse.scm -o libparse.o
+csc -static -O5 libparse.o main.o -o bin/bib2json
+csc -static -O5 libparse.o server.o -o bin/bib2jsonserver
+rm *.o
