b6e5b293832a5002f1895804aab34fa5eb33c284
Dmitry Nikolaev <dnikolaev@fastmail.com>
2020-11-06 00:47:05 +0200

    Separate processed and unprocessed records into two lists. Create a new hash-table for each invocation of get-records-from-port.

--- src/libparse.scm
+++ src/libparse.scm
@@ -6,16 +6,15 @@
 	(chicken string)
 	(chicken port)
 	utf8
-	utf8-srfi-13
-	utf8-srfi-14
-	srfi-69
+	utf8-srfi-13			; string routines
+	utf8-srfi-14			; charsets
+	srfi-12				; exception handling
+	srfi-69				; hash tables
 	json)
 
 
-(define defined-strings (make-hash-table))
 
-
-;;; parse-bibtex-record: String -> HashTable(String, SchemeValue)
+;;; parse-bibtex-record: String x HashTable(Symbolx,String) -> HashTable(String, SchemeValue)
 ;;; usage: (parse-bibtex-record) parses an individual record into the
 ;;; following representation (in JSON notation):
 ;;; {
@@ -38,25 +37,29 @@
 ;;; and have balanced curly braces inside them. The procedure therefore 
 ;;; assumes that the prefix is well-formed. It also strips the final } or )
 (define parse-bibtex-record
-  (lambda (str)
-    (call-with-input-string
-	(let ([no-whitespace (string-trim-both str)])
-	  (substring no-whitespace
-		     0
-		     (sub1 (string-length no-whitespace))))
-      (lambda (port)
-	(get-record-type port
-			 (make-hash-table))))))
+  (lambda (str defined-strings)
+    (let ([no-whitespace (string-trim-both str)])
+      (call-with-input-string
+	(substring no-whitespace
+		   0
+		   (sub1 (string-length no-whitespace)))
+	(lambda (port)
+	  (get-record-type port
+			   (make-hash-table)
+			   no-whitespace
+			   defined-strings))))))
 
 
 ;;; get-record-type: Port x
-;;;                  HashMap(String, SchemeValue) x
-;;;                  -> HashMap(String, SchemeValue)
+;;;                  HashMap(Symbol, SchemeValue) x
+;;;                  String x
+;;;                  HashMap(Symbol, String)
+;;;                  -> HashMap(Symbol, SchemeValue)
 ;;; usage: (get-record-type port result buffer c stage) adds the type of
 ;;; bibtex record to the hash table and calls the procedure that extracts
 ;;; the fields.
 (define get-record-type
-  (lambda (port result)
+  (lambda (port result original-string defined-strings)
     (let loop ([c (read-char port)]
 	       [buffer '()])
       (cond [(or (eqv? c #\{)
@@ -67,7 +70,9 @@
 				     'entry-type
 				     (reverse-list->string buffer))
 		    (get-record-fields port
-				       result))]
+				       result
+				       original-string
+				       defined-strings))]
 	    [(eqv? c #\@)
 	     (loop (read-char port)
 		   buffer)]
@@ -82,7 +87,7 @@
 ;;; usage: (get-record-fields port result buffer) fills the
 ;;; description of the bibtex entry.
 (define get-record-fields
-  (lambda (port result)
+  (lambda (port result original-string defined-strings)
     ;; We begin by extracting the cite key, which
     ;; is a string of characters before the comma.
     ;; This will break if there is an escaped comma
@@ -112,18 +117,20 @@
 		   result)
 	    (let ([field-name next-token])
 	      ;; Extract the value.
-	      (let inner-loop ([next-token (read-word port)]
+	      (let inner-loop ([next-token (read-word port original-string)]
 			       [buffer '()])
 		(cond [(symbol? next-token) ; Try looking up in the hash table.
-		       (inner-loop (read-word port)
+		       (inner-loop (read-word port original-string)
 				   (cons (hash-table-ref defined-strings next-token
 							 (lambda ()
 							   (symbol->string next-token)))
 					 buffer))]
 		      [(eof-object? next-token) ; End of fields.
 		       (if (null? buffer)
-			   (error (format "An empty value in the field ~A"
-					  field-name))
+			   (abort `(parse-error
+				    ,(format "~S~%An empty value in the field ~A"
+					     original-string
+					     field-name)))
 			   (begin (hash-table-set! description
 						   (string->symbol field-name)
 						   (string-concatenate (reverse buffer)))
@@ -133,8 +140,10 @@
 				  result))]
 		      [(string=? next-token ",") ; Move on to the next field.
 		       (if (null? buffer)
-			   (error (format "An empty value in the field ~A"
-					  field-name))
+			   (abort `(parse-error
+				    ,(format "~S~%An empty value in the field ~A"
+					     original-string
+					     field-name)))
 			   (begin (hash-table-set! description
 						   (string->symbol field-name)
 						   (string-concatenate (reverse buffer)))
@@ -144,20 +153,20 @@
 						port)))))]
 		      
 		      [(string=? "#" next-token)
-		       (inner-loop (read-word port)
+		       (inner-loop (read-word port original-string)
 				   buffer)]
-		      [else (inner-loop (read-word port)
+		      [else (inner-loop (read-word port original-string)
 					(cons next-token buffer))]))))))))
 
 
-;;; define-string: String -> undefined
-;;; usage: (define-string str) parses a bibtex record
+;;; define-string: String x HashTable(Symbol,String) -> undefined
+;;; usage: (define-string str defined-strings) parses a bibtex record
 ;;; of the form @string[({] varname = "value" [})] and sets
 ;;; the value of 'varname in defined-strings to the value.
 (define define-string
-  (lambda (str)
+  (lambda (str defined-strings)
     (if (not (string-prefix-ci? "@string" str))
-	(error (format "Wrong record type:~%~A" str))
+	(abort `(definition-error ,(format "Wrong record type:~%~A" str)))
 	(let* ([no-prefix (substring str
 				     (string-length "@string "))]
 	       [no-quotechar (substring no-prefix
@@ -168,12 +177,15 @@
 			    (split-once no-whitespace "="))])
 	       (if (eqv? (length fields)
 			 1)
-		   (error (format "No assignment found:~%~A" str))
+		   (abort `(definition-error ,(format "No assignment found:~%~A" str)))
 		   (hash-table-set! defined-strings
 				    (string->symbol (car fields))
 				    (call-with-input-string
 					(cadr fields)
-				      concatenate-parts)))))))
+				      (lambda (port) (concatenate-parts
+						      port
+						      str
+						      defined-strings)))))))))
 
 
 ;;; split-once: String x String | Character -> Listof(String)
@@ -200,7 +212,7 @@
 	   (error "The delimiter must be either a character or a string.")])))
 
 
-;;; concatenate-parts: port -> String
+;;; concatenate-parts: Port x HashTable(String,String) -> String
 ;;; usage: (concatenate-parts port) converts a string of the form
 ;;; "part1" [# " part2"]... to "part1 part2". Unlike the vanilla
 ;;; bibtex, it permits concatenation of tokens delimited with {}
@@ -211,29 +223,36 @@
 ;;; Should be invoked in an (call-with-input-string str concatenate-parts)
 ;;; context.
 (define concatenate-parts
-  (lambda (port)
-    (let loop ([next-token (read-word port)]
+  (lambda (port original-string defined-strings)
+    (let loop ([next-token (read-word port
+				      original-string)]
 	       [res '()])
       (cond [(eof-object? next-token)
 	     (string-concatenate (reverse res))]
 	    [(symbol? next-token)
-	     (loop (read-word port)
+	     (loop (read-word port
+			      original-string)
 		   (cons (hash-table-ref defined-strings next-token)
 			 res))]
 	    [(string=? next-token "#")
-	     (loop (read-word port) res)]
+	     (loop (read-word port
+			      original-string)
+		   res)]
 	    [(string=? next-token ",")
-	     (error "A comma in the string variable.")]
-	    [else (loop (read-word port)
+	     (abort `(parse-error "A comma in the string variable."))]
+	    [else (loop (read-word port
+				   original-string)
 			(cons next-token res))]))))
 
 
-;;; read-word: Port -> String | Symbol
-;;; usage: (read-word port) returns the next word contained between
-;;; "" or {}, #f if the port is empty or an error if the
-;;; next sequence of tokens is ill formed.
+;;; read-word: Port x String -> String | Symbol
+;;; usage: (read-word port original-string)
+;;; returns the next word contained between
+;;; "" or {} or #f if the port is empty and
+;;; raises an exception if the next sequence of tokens
+;; is ill formed.
 (define read-word
-  (lambda (port)
+  (lambda (port original-string)
     (let loop ([c (read-char port)]
 	       [res '()]
 	       [after-backslash #f]
@@ -249,14 +268,20 @@
 		; Return the variable name without consuming the comma
 		(string->symbol (reverse-list->string (cons c res))))
 	    ; The basic mode
-	    (cond [(< depth 0) (error (format "Unbalanced curly braces in the record:~%~C~A"
-					      quotechar
-					      (reverse-list->string res)))]
+	    (cond [(< depth 0)
+		   (abort
+		    `(parse-error
+		      ,(format "~S~%Unbalanced curly braces in the record:~%~C~A"
+			       original-string
+			       quotechar
+			       (reverse-list->string res))))]
 		  [(eof-object? c)	     
 		   (cond [quotechar
-			  (error (format "An unfinished token:~%~C~A"
-					 quotechar
-					 (reverse-list->string res)))]
+			  (abort `(parse-error
+				   ,(format "~S~%An unfinished token:~%~C~A"
+					    original-string
+					    quotechar
+					    (reverse-list->string res))))]
 			 [else
 			  (if (null? res)
 			      c
@@ -278,7 +303,8 @@
 				    after-backslash
 				    quotechar
 				    depth)
-			      (string->symbol (reverse-list->string res)))] ; Return a variable name
+			      ; Return a variable name
+			      (string->symbol (reverse-list->string res)))] 
 			 [(or (eqv? c #\") (eqv? c #\{)) ; Start collecting a token
 			  (let ([start-depth (if (eqv? c #\") 0 1)])
 			    (if (null? res)
@@ -287,13 +313,17 @@
 				      after-backslash
 				      c
 				      start-depth)
-				(error (format "A malformed string literal: ~A"
-					       (reverse-list->string (cons c res))))))]
+				(abort `(parse-error
+					 ,(format "~S~%A malformed string literal: ~A"
+						  original-string
+						  (reverse-list->string (cons c res)))))))]
 			 [(eqv? c #\#)	; Return token separator
 			  (if (null? res)
 			      "#"
-			      (error (format "An illegal variable name: ~A"
-					     (reverse-list->string (cons c res)))))]
+			      (abort `(parse-error
+				       ,(format "~S~%An illegal variable name: ~A"
+						original-string
+						(reverse-list->string (cons c res))))))]
 			 [(eqv? c #\,)
 			  ","]
 			 [(or (char-set-contains? char-set:letter c)
@@ -304,8 +334,10 @@
 				quotechar
 				depth)]
 			 [else
-			  (error (format "An illegal character outside a token: ~C"
-					 c))])]
+			  (abort `(parse-error
+				   ,(format "~S~%An illegal character outside a token: ~C"
+					    original-string
+					    c)))])]
 		  [else			; Inside-token mode
 		   (cond [(eqv? c #\{)	; Cases where the quotechar choice is irrelevant
 			  (loop (read-char port)
@@ -329,9 +361,12 @@
 					   (sub1 depth))]
 				    [(eqv? c #\")
 				     (if (> depth 0)
-					 (error (format "Unbalanced curly braces in the record:~%~A"
-							(reverse-list->string res)))
-					 (reverse-list->string res))]		     
+					 (abort
+					  `(parse-error
+					    ,(format
+					      "~S~%Unbalanced curly braces in the record:~%~A"
+					      original-string
+					      (reverse-list->string res))))		 					 (reverse-list->string res))]		     
 				    [else (loop (read-char port)
 						(cons c res)
 						after-backslash
@@ -383,11 +418,15 @@
     (let loop ([c (read-char port)]
 	       [res res])
       (cond [(eof-object? c)
-	     (error "An incomplete record at the end of input.")]
+	     (abort
+	      `(parse-error
+		,(format "An incomplete record at the end of input:~%~A"
+			 (reverse-list->string res))))]
 	    [(and (or (eqv? c #\() (eqv? c #\{))
 		  (eqv? (length res) 1))
-	     (error (string-concatenate '("An entry without a type was encountered."
-					  " Check for '@{' or '@(' in the input.")) )]
+	     (abort `(parse-error
+		      ,(string-concatenate '("An entry without a type was encountered."
+					  " Check for '@{' or '@(' in the input."))))]
 	    [(eqv? c #\{)
 	     (get-entry-body (cons c res)
 			     c  ; The outer brace type
@@ -408,7 +447,9 @@
 	     (loop (read-char port) res)]
 	    [(char-set-contains? char-set:letter c)
 	     (loop (read-char port) (cons c res))]
-	    [else error (format "A non-letter character in the entry type: ~S." c)]))))
+	    [else
+	     (abort `(parse-error
+		      ,(format "A non-letter character in the entry type: ~S." c)))]))))
 
 
 ;;; get-entry-body: ListOf(Character) x Bool x Bool x Port -> String
@@ -427,10 +468,13 @@
 	       [inside-quotes inside-quotes]
 	       [after-backslash after-backslash])
       (cond [(eof-object? c)
-	     (error "An incomplete record at the end of input.")]
+	     (abort `(parse-error
+		      ,(format "An incomplete record at the end of input:~%~S"
+			       (reverse-list->string res))))]
 	    [(< embedding-level 0)
-	     (error (format "Unbalanced braces in the record:~%~S"
-			    (reverse-list->string res)))]
+	     (abort `(parse-error
+		      ,(format "Unbalanced braces in the record:~%~S"
+			       (reverse-list->string res))))]
 	    [after-backslash		; Just add the character
 	     (loop (read-char port)
 		   (cons c res)
@@ -442,8 +486,9 @@
 		  (not inside-quotes)
 		  (not (eqv? outer-delimiter #\())
 		  (eqv? embedding-level 1))
-	     (error (format "An erroneus @ inside a record or an incomplete record:~%~S"
-			    (reverse-list->string (cons c res))))]
+	     (abort `(parse-error
+		      ,(format "An erroneus @ inside a record or an incomplete record:~%~S"
+			    (reverse-list->string (cons c res)))))]
 	    ;; Unconditionally add the next character after a backslash
 	    ;; if the embedding level is right or we're between
 	    ;; quote marks. Signal an error otherwise.
@@ -455,8 +500,9 @@
 		       embedding-level
 		       inside-quotes
 		       #t)
-		 (error (format "Backslash outside a field:~%~S"
-				(reverse-list->string (cons c res)))))]
+		 (abort `(parse-error
+			  ,(format "Backslash outside a field:~%~S"
+				(reverse-list->string (cons c res))))))]
 	    [(or			; Are we done?
 	      ;; Outer paren case
 	      (and (eqv? outer-delimiter #\( )
@@ -522,17 +568,27 @@
 
 ;;; TODO: docstring
 (define (get-records-from-port port)
-	(let loop ([res '()])
-	  (let ([record (get-bibtex-entry port)])
-	    (cond [(not record)
-		   		(reverse res)]		  
-		  	  [(string-prefix-ci? "@preamble" record)
-		        (loop res)]
-		      [(string-prefix-ci? "@string" record)
-		        (begin (define-string record)
-			           (loop res))]
-		  	  [else
-		   	     (loop (cons (parse-bibtex-record record) res))]))))
-
-
-
+  (let ([defined-strings (make-hash-table)])
+    (let loop ([done '()]
+	       [failed '()])
+      (handle-exceptions exn
+	  (cond [(eqv? (car exn) 'parse-error)
+		 (loop done
+		       (cons (cadr exn) failed))]
+		[(eqv? (car exn) 'definition-error)
+		 (loop done
+		       (cons (cadr exn) failed))]
+		[else (abort exn)])
+	(let ([record (get-bibtex-entry port)])
+	  (cond [(not record)
+		 (list (reverse done) (reverse failed))]		  
+		[(string-prefix-ci? "@preamble" record)
+		 (loop done failed)]
+		[(string-prefix-ci? "@string" record)
+		 (begin (define-string record defined-strings)
+			(loop done failed))]
+		[else
+		 (loop (cons (parse-bibtex-record record
+						  defined-strings)
+			     done)
+		       failed)]))))))
